e89fab19c78f974062a0144b1e1746971a36fd33
PonponJuice <ponjuice2021@gmail.com>
2025-06-21 14:04:00 +0900

    :sparkles: GameInfoクラスと関連する型を追加

---
+++ client/src/lib/gameLogic.ts
@@ -0,0 +1,75 @@
+import type { Card, GameEvent, PlayerInfo } from "./type";
+
+class GameInfo {
+    gameId: string;
+    playerId: number;
+    fieldCards: Card[];
+
+    players: PlayerInfo[];
+    
+    currentPlayerId: number;
+    turn: number;
+    turnTimeRemaining: number;
+
+    constructor(gameId: string, playerId: number) {
+        this.gameId = gameId;
+        this.playerId = playerId;
+        this.fieldCards = [];
+        this.players = [
+            { id: 0, score: 0, handsLimit: 0, cards: [], expression: '' },
+            { id: 1, score: 0, handsLimit: 0, cards: [], expression: '' }
+        ];
+        this.currentPlayerId = -1;
+        this.turn = 0;
+        this.turnTimeRemaining = 0;
+    }
+
+    useCard(cardId: string): void {}
+    addOperator(operator: '(' | ')'): void {}
+    deleteExpr(): void {}
+
+    onEvent(event: GameEvent): void {
+        switch (event.type) {
+            case "gameReady":
+                this.handleGameReady(event);
+                break;
+            case "gameStarted":
+                this.handleGameStarted(event);
+                break;
+            case "turnStarted":
+                this.handleTurnStarted(event);
+                break;
+            case "cardsUpdated":
+                this.handleCardsUpdated(event);
+                break;
+            case "turnTimeRemainingChanged":
+                this.handleTurnTimeRemainingChanged(event);
+                break;
+            case "submissionSucceeded":
+                this.handleSubmissionSucceeded(event);
+                break;
+            case "scoreUpdated":
+                this.handleScoreUpdated(event);
+                break;
+            case "turnEnded":
+                this.handleTurnEnded(event);
+                break;
+            case "gameEnded":
+                this.handleGameEnded(event);
+                break;
+            default:
+                console.warn("Unhandled game event:", event);
+        }
+    }
+
+    private handleGameReady(event: GameEvent): void {}
+    private handleGameStarted(event: GameEvent): void {}
+    private handleTurnStarted(event: GameEvent): void {}
+    private handleCardsUpdated(event: GameEvent): void {}
+    private handleTurnTimeRemainingChanged(event: GameEvent): void {}
+    private handleSubmissionSucceeded(event: GameEvent): void {}
+    private handleScoreUpdated(event: GameEvent): void {}
+    private handleTurnEnded(event: GameEvent): void {}
+    private handleGameEnded(event: GameEvent): void {}
+
+}
---
+++ client/src/lib/type.ts
@@ -0,0 +1,83 @@
+export type Card = {
+    id: string;
+    type: string;
+    value: string | number;
+}
+
+export type GameReady = {
+    type: "gameReady";
+    fieldCards: Card[];
+    player0: Card[];
+    player0HandsLimit: number;
+    player1: Card[];
+    player1HandsLimit: number;
+    currentPlayerId: number;
+    player0Score: number;
+    player1Score: number;
+    startTime: string;
+}
+
+export type GameStarted = {
+    type: "gameStarted";
+    currentPlayerId: number;
+    turn: number;
+}
+
+export type TurnStarted = {
+    type: "turnStarted";
+    currentPlayerId: number;
+    turn: number;
+    turnTimeRemaining: number;
+}
+
+export type CardsUpdated = {
+    type: "cardsUpdated";
+    fieldCards: Card[];
+    player0: Card[];
+    player0HandsLimit: number;
+    player1: Card[];
+    player1HandsLimit: number;
+}
+
+export type TurnTimeRemainingChanged = {
+    type: "turnTimeRemainingChanged";
+    currentPlayerId: number;
+    remainingSeconds: number;
+}
+
+export type SubmissionSucceeded = {
+    type: "submissionSucceeded";
+    playerId: number;
+    expression: string;
+    score: number;
+}
+
+export type ScoreUpdated = {
+    type: "scoreUpdated";
+    player0: number;
+    player1: number;
+}
+
+export type TurnEnded = {
+    type: "turnEnded";
+    nextPlayerId: number;
+    nextTurn: number;
+}
+
+export type GameEnded = {
+    type: "gameEnded";
+    player0: number;
+    player1: number;
+}
+
+export type GameEvent =
+    GameReady | GameStarted | TurnStarted | CardsUpdated | TurnTimeRemainingChanged |
+    SubmissionSucceeded | ScoreUpdated | TurnEnded | GameEnded;
+
+export type PlayerInfo = {
+    id: number;
+    score: number;
+    cards: Card[];
+    handsLimit: number;
+    expression: string;
+}
