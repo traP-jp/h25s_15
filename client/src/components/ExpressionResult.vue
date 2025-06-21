7019edbb80b03bff435db5332076cec91f7c9bb8
irinoirino <genkishoot@icloud.com>
2025-06-21 13:21:21 +0900

    基本css追加

--- client/src/components/ExpressionResult.vue
+++ client/src/components/ExpressionResult.vue
@@ -1,7 +1,12 @@
 <template>
-<div class=""></div>
+<div class="exp-box"></div>
 </template>
 
 <style>
-    
+    .exp-box{
+        height:24.875rem;
+        width:102.6875rem;
+        display: flex;
+        background-color:rgba(0,0,0,0.25);
+    }
 </style>
