d6841c158269a13be5af358d394e6ef1724ad329
irinoirino <genkishoot@icloud.com>
2025-06-21 13:58:54 +0900

    format,lint

--- client/src/components/ExpressionResult.vue
+++ client/src/components/ExpressionResult.vue
@@ -1,13 +1,12 @@
 <template>
-<div class="exp-box"></div>
-
+  <div class="exp-box"></div>
 </template>
 
 <style scoped>
-    .exp-box{
-      height:24.875rem;
-      width:102.6875rem;
-      display:flex;
-      background-color:rgba(0,0,0,0.25);
-    }
+.exp-box {
+  height: 24.875rem;
+  width: 102.6875rem;
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.25);
+}
 </style>
