acc5a9da8f34308bb439e67c46f5ea36a0d4dba4
PonponJuice <ponjuice2021@gmail.com>
2025-06-21 10:30:50 +0900

    GameCardコンポーネントを追加

---
+++ client/src/components/GameCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const props = defineProps<{
+  charactor: string
+  selected: boolean
+  disabled?: boolean
+  size?: 'small' | 'medium' | 'large'
+}>()
+
+const emit = defineEmits(['click'])
+
+function onClick() {
+  emit('click')
+}
+
+const size = props.size || 'medium'
+</script>
+
+<template>
+  <button
+    class="card"
+    :disabled="props.selected || props.disabled"
+    :style="{
+      backgroundColor: props.selected ? '#636363' : '#FFFFFF',
+      width: size === 'small' ? '2rem' : size === 'medium' ? '4rem' : '5rem',
+      fontSize: size === 'small' ? '2rem' : size === 'medium' ? '4rem' : '5rem',
+      border: 'none',
+      aspectRatio: 3 / 4,
+    }"
+    @click="onClick"
+  >
+    {{ props.charactor }}
+  </button>
+</template>
+
+<style scoped>
+.card {
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  color: var(--theme-text-black);
+  border-radius: 4px;
+}
+</style>
