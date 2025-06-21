cb9ac1914d948da948e660bacfc7fd7e3ec9806d
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-06-21 12:27:56 +0900

    :adhesive_bandage: Errorfにする

--- server/internal/cards/clear.go
+++ server/internal/cards/clear.go
@@ -20,7 +20,7 @@ func (h Handler) ClearHandCards(c echo.Context) error {
 
 	userName, err := users.GetUserName(c)
 	if err != nil {
-		c.Logger().Printf("failed to get user name: %v", err)
+		c.Logger().Errorf("failed to get user name: %v", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get user name")
 	}
 
