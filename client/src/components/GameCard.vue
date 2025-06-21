cb16cc21b4483de516a2912f7aa9ca94b1a7f6eb
ponjuice <99527981+PonponJuice@users.noreply.github.com>
2025-06-21 11:14:42 +0900

    GameCardコンポーネントを追加 (#34)
    * GameCardコンポーネントを追加
    
    * fix typo
    
    * 中身を文字以外も取れるように変更
    
    * pnpm format
    
    * サイズの変更
    
    * cardのradiusの修正
    
    * 内部の文字の大きさを変更
    
    * border:none の修正

---
+++ client/src/components/GameCard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const props = defineProps<{
+  selected?: boolean
+  disabled?: boolean
+  size?: 'small' | 'medium' | 'large'
+}>()
+
+const emit = defineEmits(['click'])
+
+function onClick() {
+  emit('click')
+}
+
+const size = props.size || 'medium'
+const sizeValue = size === 'small' ? '4rem' : size === 'medium' ? '6.25rem' : '7.5rem'
+</script>
+
+<template>
+  <button
+    class="card"
+    :disabled="props.selected || props.disabled"
+    :style="{
+      backgroundColor: props.selected ? '#636363' : '#FFFFFF',
+      width: sizeValue,
+      fontSize: sizeValue,
+    }"
+    @click="onClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.card {
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  color: var(--theme-text-black);
+  border-radius: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  aspect-ratio: 3 / 4;
+}
+</style>
