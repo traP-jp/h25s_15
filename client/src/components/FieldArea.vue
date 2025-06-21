4b83975f92e0fd8d74654a802eb6fec408e122ba
irinoirino <genkishoot@icloud.com>
2025-06-21 12:18:25 +0900

    名前変更

---
+++ client/src/components/FieldArea.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="expr-box"><slot /></div>
+</template>
+
+<style>
+.expr-box {
+  min-width: 41.5rem;
+  height: 14.6875rem;
+  padding: 1.875rem;
+  background: #3f8773;
+  border: 0.375rem #256b57 solid;
+  display: flex;
+  gap: 2.5rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
