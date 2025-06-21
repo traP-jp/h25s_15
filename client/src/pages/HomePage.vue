ec10a37fa638c4b7e5e6ead0a6e600b6881b3655
tufusa <me@tufusa.net>
2025-06-21 15:40:45 +0900

    Revert "スコア表示画面を作成しました" (#75)

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import HelloWorld from '../components/HelloWorld.vue'
-import PointResult from '../components/PointResult.vue'
 </script>
 
 <template>
   <hello-world msg="Hello Vue 3 + Vite" />
-  <PointResult />
 </template>
