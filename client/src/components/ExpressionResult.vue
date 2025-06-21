f0c27d3d30b73e186edfcc9bcec80c3fa30738e7
irinoirino <genkishoot@icloud.com>
2025-06-21 13:45:17 +0900

    保存用

--- client/src/components/ExpressionResult.vue
+++ client/src/components/ExpressionResult.vue
@@ -1,12 +1,15 @@
 <template>
 <div class="exp-box"></div>
+
 </template>
 
-<style>
+<style scoped>
     .exp-box{
-        height:24.875rem;
-        width:102.6875rem;
-        display: flex;
-        background-color:rgba(0,0,0,0.25);
+      height:24.875rem;
+      width:102.6875rem;
+      font-size:12px;
+      display:block;
+      background-color:rgba(0,0,0,0.25);
+      margin:0 auto;
     }
 </style>
--- client/src/pages/GamePage.vue
+++ client/src/pages/GamePage.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ExpressionResult from '@/components/ExpressionResult.vue';
+</script>
 
 <template>
   <h1>this is GamePage</h1>
+  <ExpressionResult/>
+  
 </template>
