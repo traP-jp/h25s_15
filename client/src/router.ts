d0398266b74433d8d0b3bacffe4f2de586068aa6
ysaga <mohomohoushi@gmail.com>
2025-06-21 10:07:15 +0900

    対戦結果ページを作りました

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './pages/HomePage.vue' // 仮
+import ResultPage from './pages/ResultPage.vue'
 
-const routes = [{ path: '/', name: 'home', component: HomePage }]
+const routes = [
+  { path: '/', name: 'home', component: HomePage },
+  { path: '/result/:gameId', name: 'resultpage', component: ResultPage },
+]
 
 const router = createRouter({
   history: createWebHistory(),
