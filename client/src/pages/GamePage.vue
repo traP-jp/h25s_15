76d1cede0aa784bf44598b857781aa19da608de1
irinoirino <genkishoot@icloud.com>
2025-06-21 14:27:15 +0900

    表示欄を二分割した

--- client/src/pages/GamePage.vue
+++ client/src/pages/GamePage.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ExpressionResult from '@/components/ExpressionResult.vue';
+</script>
 
 <template>
   <h1>this is GamePage</h1>
+  <ExpressionResult/>
 </template>
