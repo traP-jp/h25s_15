ba3c3f53b134fbcc9c9b4c7d05b6a9baf3e655c9
入野拳樹 <genkishoot@icloud.com>
2025-06-21 11:15:15 +0900

    paren追加 (#35)
    * paren追加
    
    * name変更
    
    * scoped 追加
    
    * name変更再び
    
    * weight編集
    
    * remに変換
    
    * style編集

---
+++ client/src/components/ParenthesisButtons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="paren-row">
+    <button class="paren">(</button>
+    <button class="paren">)</button>
+  </div>
+</template>
+
+<style scoped>
+.paren-row {
+  display: flex;
+  justify-content: center;
+  gap: 3.125rem;
+}
+.paren {
+  background: var(--theme-secondary);
+  color: var(--theme-text-black);
+  font-size: 3rem;
+  width: 8.0625rem;
+  height: 4.375rem;
+  border-radius: 3.125rem;
+  border: none;
+  font-weight: 400;
+  padding: 0.3125rem 3.125rem;
+}
+</style>
