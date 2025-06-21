2948131d153f62c12dfbe6116df608655908061d
ysaga <sagara.y.4991@m.isct.ac.jp>
2025-06-21 18:23:56 +0900

    ホーム画面のレイアウト (#92)

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,7 +1,39 @@
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue'
+import CommonButton from '@/components/CommonButton.vue'
 </script>
 
 <template>
-  <hello-world msg="Hello Vue 3 + Vite" />
+  <div class="game-title">（タイトル）</div>
+  <CommonButton class="game-start">ゲームを始める</CommonButton>
+  <div class="square"></div>
 </template>
+
+<style scoped>
+.game-title {
+  font-size: 8rem;
+  text-align: center;
+  display: block;
+  padding-top: 5rem;
+  margin-bottom: 4rem;
+}
+
+.game-start {
+  color: var(--theme-text-white);
+  background-color: var(--theme-primary);
+  display: flex;
+  font-size: 3.25rem;
+  width: 28.9375rem;
+  height: 7.0625rem;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+}
+
+.square {
+  background-color: var(--theme-surface);
+  width: 97.625rem;
+  height: 34.0625rem;
+  align-items: center;
+  margin-top: 4rem;
+}
+</style>
