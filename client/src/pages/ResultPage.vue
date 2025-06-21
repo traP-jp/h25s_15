135dfb5ab0a0a51aa24eccfb8ff70f5e16f3cfe7
irinoirino <genkishoot@icloud.com>
2025-06-21 19:07:25 +0900

    仮設定

--- client/src/pages/ResultPage.vue
+++ client/src/pages/ResultPage.vue
@@ -3,53 +3,54 @@ import CommonButton from '@/components/CommonButton.vue'
 import ExpressionResult from '@/components/ExpressionResult.vue'
 import PointResult from '@/components/PointResult.vue'
 
-// import { ref, onMounted } from 'vue'
-// const props = defineProps<{
-//   gameId: string;
-// }>()
+import { ref, onMounted } from 'vue'
+const props = defineProps<{
+  gameId: string
+}>()
 
-// const meInfo = ref()
-// const resultInfo = ref()
-// const isWin = ref()
+const meInfo = ref()
+const resultInfo = ref()
+const isWin = ref()
 
-// onMounted(async () => {
+onMounted(async () => {
+  //自分のnameを取得
+  const meRes = await fetch('/users/me')
+  if (meRes.ok) {
+    meInfo.value = await meRes.json()
+  }
 
-//   //自分のnameを取得
-//   const meRes = await fetch('/users/me')
-//   if (meRes.ok) {
-//     meInfo.value = await meRes.json()
-//   }
+  //ゲームの結果を取得
+  const resultRes = await fetch('/games/' + props.gameId + '/results')
+  if (resultRes.ok) {
+    resultInfo.value = await resultRes.json()
+  }
 
-//   //ゲームの結果を取得
-//   const resultRes = await fetch('/games/:gameId/results')
-//   if (resultRes.ok) {
-//     resultInfo.value = await resultRes.json()
-//   }
-
-//   //勝ち負けを判定し、isWinに代入
-//   if(resultInfo.value.player0Name == meInfo.value.name){
-//     if(resultInfo.value.player0Score > resultInfo.value.player1Score){
-//       isWin.value = "VICTORY";
-//     }else if(resultInfo.value.player0Score < resultInfo.value.player1Score){
-//       isWin.value = "LOSE";
-//     }else{
-//       isWin.value = "DRAW"
-//     }
-//   }else{
-//     if(resultInfo.value.player0Score > resultInfo.value.player1Score){
-//       isWin.value = "LOSE";
-//     }else if(resultInfo.value.player0Score < resultInfo.value.player1Score){
-//       isWin.value = "VICTORY";
-//     }else{
-//       isWin.value = "DRAW"
-//     }
-//   }
-// })
+  //勝ち負けを判定し、isWinに代入
+  if (resultInfo.value.player0Name == meInfo.value.name) {
+    if (resultInfo.value.player0Score > resultInfo.value.player1Score) {
+      isWin.value = 'VICTORY'
+    } else if (resultInfo.value.player0Score < resultInfo.value.player1Score) {
+      isWin.value = 'LOSE'
+    } else {
+      isWin.value = 'DRAW'
+    }
+  } else {
+    if (resultInfo.value.player0Score > resultInfo.value.player1Score) {
+      isWin.value = 'LOSE'
+    } else if (resultInfo.value.player0Score < resultInfo.value.player1Score) {
+      isWin.value = 'VICTORY'
+    } else {
+      isWin.value = 'DRAW'
+    }
+  }
+})
 </script>
 
 <template>
+  <!-- あとでVICTORYを{{isWin}}に変更する -->
   <div class="isJugded">VICTORY</div>
 
+  <!-- あとでUser情報を:user1="{ name: resultInfo.player0Name, score: resultInfo.player0Score }" :user2="{ name: resultInfo.player1Name, score: resultInfo.player1Score }"に変更する -->
   <PointResult
     :user1="{ name: 'Ponjuice', score: 20 }"
     :user2="{ name: 'ikura-hamu', score: 30 }"
@@ -60,6 +61,7 @@ import PointResult from '@/components/PointResult.vue'
     <CommonButton size="large" theme="secondary" variant="outline">ホームに戻る</CommonButton>
   </div>
 
+  <!-- あとでExpressions情報を:myExpressions="resultInfo.player0SuccessExpressions" :opponentExpressions="resultInfo.player1SuccessExpressions"に変更する -->
   <ExpressionResult
     :myExpressions="['3+7=10', '2×4+2=10']"
     :opponentExpressions="['10=4+6', '10=2×4+2']"
