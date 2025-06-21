d229a0d50435be9fad58a3df59b0c7bc8762d2e8
irinoirino <genkishoot@icloud.com>
2025-06-21 10:38:55 +0900

    paren追加

---
+++ client/src/components/ParenUI.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="paren-row">
+    <button class="paren">(</button>
+    <button class="paren">)</button>
+  </div>
+</template>
+
+<style>
+.paren-row {
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+}
+.paren {
+  background: var(--theme-secondary);
+  color: var(--theme-text-black);
+  font-size: 48px;
+  width: 129px;
+  height: 70px;
+  border-radius: 50px;
+  border: none;
+  font-weight: 500;
+  margin: 0 4px;
+}
+</style>
