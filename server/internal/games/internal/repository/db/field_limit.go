fd87e6020093f74ce0b4c462eaa360df3b6428f1
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-06-21 23:17:34 +0900

    :bug: SQL修正

--- server/internal/games/internal/repository/db/field_limit.go
+++ server/internal/games/internal/repository/db/field_limit.go
@@ -10,11 +10,8 @@ import (
 const initialFieldCardsLimit int = 4
 
 func (r *Repo) InitializeFieldCardsLimit(ctx context.Context, gameID uuid.UUID) error {
-	_, err := r.db.DB(ctx).ExecContext(ctx, `
-		UPDATE field_cards_limits
-		SET field_cards = ?
-		WHERE game_id = ?
-	`, initialFieldCardsLimit, gameID)
+	_, err := r.db.DB(ctx).ExecContext(ctx, "INSERT INTO field_cards_limits (game_id, field_cards) VALUES (?, ?)",
+		gameID, initialFieldCardsLimit)
 	if err != nil {
 		return fmt.Errorf("initialize field cards limit: %w", err)
 	}
