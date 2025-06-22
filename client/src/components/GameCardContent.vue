22ef25956a10b80dfdf0e49ffa28929803becd4f
irinoirino <genkishoot@icloud.com>
2025-06-22 14:35:57 +0900

    format

--- client/src/components/GameCardContent.vue
+++ client/src/components/GameCardContent.vue
@@ -1,29 +1,28 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const props = defineProps<{
   card: {
-    id: string;
-    type: string;
-    value: string | number;
+    id: string
+    type: string
+    value: string | number
   }
 }>()
 
 const imageUrl = ref<string | undefined>(undefined)
 
-if(props.card.type == "item"){
-  imageUrl.value = `/${props.card.value}.png`;
+if (props.card.type == 'item') {
+  imageUrl.value = `/${props.card.value}.png`
 }
-
 </script>
 
 <template>
-<span v-if="card.type == 'operand' || card.type == 'operator'">{{card.value}}</span>
-<img class="image-size" v-else-if="imageUrl" :src="imageUrl" />
+  <span v-if="card.type == 'operand' || card.type == 'operator'">{{ card.value }}</span>
+  <img class="image-size" v-else-if="imageUrl" :src="imageUrl" />
 </template>
 
 <style scoped>
-.image-size{
+.image-size {
   width: 100%;
   height: auto;
 }
