33a3bdb045b0481761ada1a29c05a4d48e078f00
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-06-22 15:03:36 +0900

    :bug: contextを修正

--- server/internal/cards/picks.go
+++ server/internal/cards/picks.go
@@ -30,7 +30,7 @@ func (h Handler) PickFieldCards(c echo.Context) error {
 
 	err = h.db.Transaction(c.Request().Context(), func(ctx context.Context) error {
 
-		player, err := h.repo.GetPlayer(c.Request().Context(), gameID, userName)
+		player, err := h.repo.GetPlayer(ctx, gameID, userName)
 		if errors.Is(err, coredb.ErrRecordNotFound) {
 			return echo.NewHTTPError(http.StatusBadRequest, "player not found")
 		}
@@ -39,13 +39,13 @@ func (h Handler) PickFieldCards(c echo.Context) error {
 			return echo.NewHTTPError(http.StatusInternalServerError)
 		}
 
-		limits, err := h.repo.GetGameHandLimit(c.Request().Context(), gameID)
+		limits, err := h.repo.GetGameHandLimit(ctx, gameID)
 		if err != nil {
 			c.Logger().Errorf("failed to get game hand limit: %v", err)
 			return echo.NewHTTPError(http.StatusInternalServerError)
 		}
 
-		cards, err := h.repo.GetPlayerHandCards(c.Request().Context(), gameID, player.PlayerID)
+		cards, err := h.repo.GetPlayerHandCards(ctx, gameID, player.PlayerID)
 		if err != nil {
 			c.Logger().Errorf("failed to get player hand cards: %v", err)
 			return echo.NewHTTPError(http.StatusInternalServerError)
@@ -60,17 +60,17 @@ func (h Handler) PickFieldCards(c echo.Context) error {
 		if err != nil {
 			return echo.NewHTTPError(http.StatusBadRequest, "no request card")
 		}
-		err = h.repo.PickFieldCards(c.Request().Context(), gameID, player.PlayerID, requestCard.CardID)
+		err = h.repo.PickFieldCards(ctx, gameID, player.PlayerID, requestCard.CardID)
 		if err != nil {
 			c.Logger().Errorf("failed to pick a field card: %v", err)
 			return echo.NewHTTPError(http.StatusInternalServerError)
 		}
 		cardID := uuid.New()
-		cardType, cardValue, err := DecideMakingCard(c.Request().Context())
+		cardType, cardValue, err := DecideMakingCard(ctx)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "couldn't decide making card")
 		}
-		err = h.repo.CreateCard(c.Request().Context(), cardID, gameID, cardType, cardValue)
+		err = h.repo.CreateCard(ctx, cardID, gameID, cardType, cardValue)
 		if err != nil {
 			c.Logger().Errorf("failed to create cards: %v", err)
 			return echo.NewHTTPError(http.StatusInternalServerError)
