51032f90160129996a3303affa90ed11b1d1ae04
tufusa <tufusa74@gmail.com>
2025-06-22 11:42:12 +0900

    chore: HandCardsの配置をslotで渡す形式に変更

--- client/src/components/HandCards.vue
+++ client/src/components/HandCards.vue
@@ -1,45 +1,20 @@
 <script setup lang="ts">
-import type { Card } from '@/lib/type'
-import GameCard from '@/components/GameCard.vue'
-
-const { cards, cardSize = 'medium' } = defineProps<{
-  cards: Card[]
+const { cardSize = 'medium' } = defineProps<{
   cardSize?: 'small' | 'medium'
 }>()
-
-const emit = defineEmits<{ (e: 'click', card: Card): void }>()
-
-const eachSlice = <T,>(array: T[], size: number): T[][] =>
-  new Array(Math.ceil(array.length / size))
-    .fill(null)
-    .map((_, i) => array.slice(i * size, (i + 1) * size))
-
-const cardColumns = eachSlice(cards, 2)
 </script>
 
 <template>
   <div class="hand-cards" :style="{ gap: cardSize == 'medium' ? '1.2rem' : '0.75rem' }">
-    <div
-      class="hand-card-rows"
-      :style="{ gap: cardSize == 'medium' ? '1.2rem' : '0.75rem' }"
-      v-for="(cardColumn, i) in cardColumns"
-      :key="`hand-card-row-${i}`"
-    >
-      <GameCard
-        v-for="card in cardColumn"
-        :size="cardSize"
-        :key="card.id"
-        :onClick="() => emit('click', card)"
-      >
-        {{ card.value }}
-      </GameCard>
-    </div>
+    <slot />
   </div>
 </template>
 
 <style scoped>
 .hand-cards {
-  display: flex;
+  display: grid;
+  grid-template-rows: repeat(2, min-content);
+  grid-auto-flow: column;
 }
 
 .hand-card-rows {
