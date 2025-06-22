8c1f60438f47386c29ed858cb76887f53b5d248c
mizunofukusayou <murakami.r.e180@m.isct.ac.jp>
2025-06-22 14:19:46 +0900

    修正

--- server/internal/items/increaseHandCardsLimit.go
+++ server/internal/items/increaseHandCardsLimit.go
@@ -12,16 +12,18 @@ import (
 func (h Handler) IncreaseHandCardsLimit(c echo.Context, gameID uuid.UUID) error {
 	userName, err := users.GetUserName(c)
 	if err != nil {
-		c.Logger().Errorf("failed to get user name: %v", err)
+		c.Logger().Errorf("failed to get user name: %w", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get user name")
 	}
 	var player domain.GamePlayer
 	player, err = h.repo.GetPlayer(c.Request().Context(), gameID, userName)
 	if err != nil {
+		c.Logger().Errorf("failed to get player: %w", err)
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 	err = h.repo.IncreaseHandCardsLimit(c.Request().Context(), gameID, player.PlayerID)
 	if err != nil {
+		c.Logger().Errorf("failed to increase hand cards limit: %w", err)
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 	return nil
